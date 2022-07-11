fec6ec1cc11a89c2ab5836f57b3472da971d1f0e
shibamoto <silverbirder@gmail.com>
2022-07-11 22:42:08 +0900

    :construction: Try graphql server

---
+++ apps/api/src/serve.ts
@@ -0,0 +1,18 @@
+import { createServer } from "@graphql-yoga/node";
+
+const server = createServer({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from Yoga!!",
+      },
+    },
+  },
+});
+
+server.start();
