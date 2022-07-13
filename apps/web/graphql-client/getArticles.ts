e152235ac55d2e3bb0ea7c5f27a570e4eac7f7b5
shibamoto <silverbirder@gmail.com>
2022-07-13 22:23:19 +0900

    :recycle: lint

--- apps/web/graphql-client/getArticles.ts
+++ apps/web/graphql-client/getArticles.ts
@@ -1,7 +1,10 @@
 import { client } from "./client";
 import { MyQueryQuery } from "./generated/graphql";
 import { readFileSync } from "fs";
-const documents = readFileSync("./graphql-client/documents/documents.graphql", "utf8");
+const documents = readFileSync(
+  "./graphql-client/documents/documents.graphql",
+  "utf8"
+);
 
 export const getArticles = async (): Promise<MyQueryQuery | undefined> => {
   const { data } = await client.query<MyQueryQuery>(documents).toPromise();
