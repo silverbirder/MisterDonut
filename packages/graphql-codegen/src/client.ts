a69e87e8e085c00191aa84fe26eb836056e9d973
shibamoto <silverbirder@gmail.com>
2022-08-07 18:36:16 +0900

    :sparkles: feat(graphql-codegen): Transform with exchanges

--- packages/graphql-codegen/src/client.ts
+++ packages/graphql-codegen/src/client.ts
@@ -1,4 +1,13 @@
-import { createClient } from "urql";
+import {
+  createClient,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+  Exchange,
+  OperationResult,
+} from "urql";
+import { pipe, tap, map } from "wonka";
+import { Donut } from "../generated/graphql";
 
 const sleep = (msec: number) =>
   new Promise((resolve) => setTimeout(resolve, msec));
@@ -11,10 +20,38 @@ const customFetch = async (
   return fetch(input, init);
 };
 
+const transformExchange: Exchange = ({ forward }) => {
+  return (ops$) =>
+    pipe(
+      ops$,
+      tap((op) => console.log("[Exchange debug]: Incoming operation: ", op)),
+      forward,
+      tap((result) =>
+        console.log("[Exchange debug]: Completed operation: ", result)
+      ),
+      // sample transform
+      map((result: OperationResult<any, any>) => {
+        if (result.data.donuts) {
+          const donuts: Donut[] = result.data.donuts;
+          result.data.donuts = donuts.map((d: Donut) =>
+            Object.assign(
+              {
+                name: d.name?.trim(),
+              },
+              d
+            )
+          );
+        }
+        return result;
+      })
+    );
+};
+
 const client = createClient({
   url: "http://localhost:4000/graphql",
   suspense: true,
   fetch: customFetch,
+  exchanges: [dedupExchange, cacheExchange, transformExchange, fetchExchange],
 });
 
 export { client };
