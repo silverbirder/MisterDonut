eb51d02e3862c8ee2d24324aa17c6481346be483
Silver-birder <silverbirder@gmail.com>
2022-08-09 13:35:51 +0900

    :sparkles: feat(ui): Add search list donuts

--- packages/ui/src/features/donuts/components/ListDonuts/ListDonuts.tsx
+++ packages/ui/src/features/donuts/components/ListDonuts/ListDonuts.tsx
@@ -2,7 +2,7 @@ import {
   useDeleteDonutMutation,
   useDonutsQuery,
 } from "@misterdonut/graphql-codegen";
-import { Suspense } from "react";
+import { ChangeEvent, Suspense, useState } from "react";
 
 type ListDonutsProps = {
   onDeleteClickHandler: () => void;
@@ -13,15 +13,39 @@ const ListDonutsInner = ({
   onDeleteClickHandler,
   renderLink,
 }: ListDonutsProps) => {
-  const [result] = useDonutsQuery();
+  const [keyword, setKeyword] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [result, executeQuery] = useDonutsQuery({
+    variables: { name: searchKeyword },
+  });
   const [, executeMutation] = useDeleteDonutMutation();
   const onDeleteClick = (id: string) => {
     executeMutation({ id: Number(id) }).then(() => {
       onDeleteClickHandler();
     });
   };
+  const onChangeKeyword = (e: ChangeEvent) => {
+    setKeyword(e.target.value);
+  };
+  const onSearchClick = () => {
+    setSearchKeyword(keyword);
+  };
+  const onRefreshClick = () => {
+    executeQuery({ requestPolicy: "network-only" });
+  };
   return (
     <>
+      <br />
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="search keyword"
+        value={keyword}
+        onChange={onChangeKeyword}
+      />
+      <button onClick={onSearchClick}>Search</button>
+      <button onClick={onRefreshClick}>Refresh</button>
       <ul>
         {result.data?.donuts?.map((d: any) => {
           return (
