312621b3da5506b5f163f495b2ba644dfe3d8c44
shibamoto <silverbirder@gmail.com>
2022-08-12 23:26:16 +0900

    :sparkles: feat(web): Add next eslint

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["custom", "prettier"],
+  extends: ["custom", "prettier", "plugin:@next/next/recommended"],
   parserOptions: {
     project: ["./tsconfig.json"],
     tsconfigRootDir: __dirname,
