8980824bc9ac85d4fef2c7a21a2ff3931675b4b3
shibamoto <silverbirder@gmail.com>
2022-08-16 22:30:35 +0900

    :sparkles: feat(storybook): Add msw to storybook

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,3 +1,11 @@
+import { setupWorker, rest } from "msw";
+
+if (typeof global.process === "undefined") {
+  const worker = setupWorker();
+  worker.start();
+  window.msw = { worker, rest };
+}
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
