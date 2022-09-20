47110ede86fe588306d534988aeff14a4436e0e9
shibamoto <silverbirder@gmail.com>
2022-09-20 21:38:01 +0900

    :white_check_mark: test: stub css

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFiles: ["./setupFile.js"],
   transform: {
     "\\.[jt]sx?$": "babel-jest",
+    ".+\\.(css)$": "jest-transform-stub",
   },
   testEnvironment: "jsdom",
 };
