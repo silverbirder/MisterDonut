e7f7affab4201a198754d205e6c706830aff24c4
shibamoto <silverbirder@gmail.com>
2022-10-01 15:39:35 +0900

    change url

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'silverbirder-misterdonut-docs.vercel.app',
+  url: 'https://silverbirder-misterdonut-docs.vercel.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
